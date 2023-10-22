372611b3e7582afcf8305c9219ef012bada313ea
WeslleySantosln <weslleys.santana299@gmail.com>
2023-10-22 15:42:39 -0300

    update

--- 06_arrays/01_aulas_arrays/scripts.js
+++ 06_arrays/01_aulas_arrays/scripts.js
@@ -8,6 +8,7 @@ console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
 console.log(arr[3]);
+
 console.log("");
 
 arr.forEach(element => {
@@ -15,10 +16,18 @@ arr.forEach(element => {
 });
 
 console.log();
+
 for(let i = 0 ; i <= arr.length ; i++){
     console.log(arr[i]);
 
-}
+};
+
+console.log();
+
+
+arr.map(cadaValor => {
+    console.log(cadaValor);
+});
 
 
 
@@ -73,18 +82,98 @@ console.log("Teto solar: " + carro.tetoSolar);
 
 
 
-
+console.log("");
 console.log("------- Carro premium: --------");
 
+let carroPremiumB = {};
 
 
+Object.assign(carroPremiumB, carro);
+Object.assign(carroPremiumB, carroPremium);
 
 
-
+console.log(carroPremiumB);
+console.log(Object.keys(carroPremiumB));
 
 
 
 //  ----------------------------- Aula 03 ------------------------  \\
 console.log("");
 console.log("//  ---------------- Aula 03-----------------------  ");
+
+let filaDoPao = ["one", "twe" , "tre" , "four" , "five" ];
+
+
+filaDoPao.forEach(fila => {
+    console.log(fila);   
+});
+
+
+//fila andou
+filaDoPao.shift(); //remove o primeiro
+console.log();
+console.log("//fila andou");
+
+
+
+filaDoPao.forEach(fila => {
+    console.log(fila);   
+});
+
+
+//chegou cliente
+filaDoPao.push("six"); //acrescenta no final
+console.log();
+console.log("//chegou cliente");
+
+
+
+filaDoPao.forEach(fila => {
+    console.log(fila); 
+});
+
+
+//Fila andou
+filaDoPao.shift();
+console.log();
+console.log("//Fila andou");
+
+filaDoPao.forEach(fila => {
+    console.log(fila); 
+});
+
+
+
+//CLIENTE VOLTOU PORQUE O TROCO TAVA ERRADO
+filaDoPao.unshift("twe");
+console.log();
+console.log("//Cliente voltou porque o troco tava errado");
+
+filaDoPao.forEach(fila => {
+    console.log(fila); 
+});
+
+
+
+//ULTIMO CLIENTE DESISTIU PORQUE TAVA DEMORANDO PRA FILA ANDAR
+filaDoPao.pop();
+console.log();
+console.log("//Ultimo cliente desistiu porque a fila tava demorando para andar");
+
+filaDoPao.forEach(fila => {
+    console.log(fila); 
+});
+
+//Fila andou
+filaDoPao.shift();
+console.log();
+console.log("//Fila andou");
+
+filaDoPao.forEach(fila => {
+    console.log(fila); 
+});
+
+//  ----------------------------- Aula 04 ------------------------  \\
+console.log("");
+console.log("//  ---------------- Aula 04-----------------------  ");
 
