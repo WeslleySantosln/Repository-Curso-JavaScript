124e457af4900991e76f27761b8bea122de0c3de
WeslleySantosln <weslleys.santana299@gmail.com>
2023-12-16 12:53:17 -0300

    update

---
+++ 11_NODE/class_2_http/index.js
@@ -0,0 +1,20 @@
+const {createServer} = require('http');
+
+let server = createServer((request,response) => {
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    
+    response.write(`
+    <h1>Hello Word!</h1>
+    <p>Primeira Página em Node.js</p>
+    `);
+
+    response.end();    
+
+});
+
+
+server.listen(8000);
+
+
+console.log("Ouvindo a porta 8000");
