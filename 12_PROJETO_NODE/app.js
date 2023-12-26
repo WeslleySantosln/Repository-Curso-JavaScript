9cdbf86db74c1bf63620e776f3ebc477917213fc
WeslleySantosln <weslleys.santana299@gmail.com>
2023-12-26 11:29:49 -0300

    instanciando handlebars e parametrizando

--- 12_PROJETO_NODE/app.js
+++ 12_PROJETO_NODE/app.js
@@ -1,5 +1,7 @@
 const express    = require('express');
+const exphbs     = require('express-handlebars');
 const app        = express();
+const path       = require("path")
 const db         = require("./db/connection.js");
 const bodyParser = require("body-parser");
 
@@ -13,6 +15,12 @@ app.listen(PORT, function(){
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false}));
+app.engine("handlebars", exphbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
+
+
+// handle bars
+app.set("views", paht.join(_dirname, "views"));
 
 
 
